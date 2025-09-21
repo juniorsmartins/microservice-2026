8812ea09cafd7a8dba1b23798f61e67f39055ccf
juniorsmartins <junior.soares.martins@proton.me>
2025-09-21 07:23:59 -0300

    FEAT: add projetos.

---
+++ api-investment/src/test/java/backend/finance/api_investment/ApiInvestmentApplicationTests.java
@@ -0,0 +1,13 @@
+package backend.finance.api_investment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiInvestmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ api-report/src/test/java/backend/finance/api_report/ApiReportApplicationTests.java
@@ -0,0 +1,13 @@
+package backend.finance.api_report;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiReportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
