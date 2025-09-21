7cd777d3fda9ccf55c86e6dead5080f29ae95c5e
juniorsmartins <junior.soares.martins@proton.me>
2025-09-21 12:39:54 -0300

    Início dos trabalhos.

---
+++ api-notifications/src/main/java/backend/communication/api_notifications/ApiNotificationsApplication.java
@@ -0,0 +1,13 @@
+package backend.communication.api_notifications;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiNotificationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiNotificationsApplication.class, args);
+	}
+
+}
---
+++ api-notifications/src/test/java/backend/communication/api_notifications/ApiNotificationsApplicationTests.java
@@ -0,0 +1,13 @@
+package backend.communication.api_notifications;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiNotificationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
