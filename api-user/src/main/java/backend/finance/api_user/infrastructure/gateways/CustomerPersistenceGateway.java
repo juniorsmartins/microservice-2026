b58e6868a61b0efda34eea9534a1c975763c28b8
juniorsmartins <junior.soares.martins@proton.me>
2025-09-25 16:56:43 -0300

    REFACTOR: aperfeiçoamento.

--- api-user/src/main/java/backend/finance/api_user/infrastructure/gateways/CustomerPersistenceGateway.java
+++ api-user/src/main/java/backend/finance/api_user/infrastructure/gateways/CustomerPersistenceGateway.java
@@ -7,7 +7,6 @@ import backend.finance.api_user.application.dtos.internal.CustomerDto;
 import backend.finance.api_user.domain.enums.RoleEnum;
 import backend.finance.api_user.infrastructure.jpas.RoleJpa;
 import backend.finance.api_user.infrastructure.ports.output.CustomerPersistenceOutputPort;
-import backend.finance.api_user.infrastructure.ports.output.CustomerQueryOutputPort;
 import backend.finance.api_user.infrastructure.presenters.CustomerPresenter;
 import backend.finance.api_user.infrastructure.repositories.CustomerRepository;
 import backend.finance.api_user.infrastructure.repositories.RoleRepository;
@@ -16,12 +15,11 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 @RequiredArgsConstructor
-public class CustomerGateway implements CustomerPersistenceOutputPort, CustomerQueryOutputPort {
+public class CustomerPersistenceGateway implements CustomerPersistenceOutputPort {
 
     private final CustomerRepository customerRepository;
 
@@ -55,27 +53,6 @@ public class CustomerGateway implements CustomerPersistenceOutputPort, CustomerQ
     @Override
     public void deleteById(UUID id) {
         customerRepository.deleteById(id);
-    }
-
-    @Transactional(readOnly = true)
-    @Override
-    public Optional<CustomerDto> findById(UUID id) {
-        return customerRepository.findById(id)
-                .map(CustomerPresenter::toCustomerDto);
-    }
-
-    @Transactional(readOnly = true)
-    @Override
-    public Optional<CustomerDto> findByEmail(String email) {
-        return customerRepository.findByEmail(email)
-                .map(CustomerPresenter::toCustomerDto);
-    }
-
-    @Transactional(readOnly = true)
-    @Override
-    public Optional<CustomerDto> findByUsername(String username) {
-        return customerRepository.findByUserUsername(username)
-                .map(CustomerPresenter::toCustomerDto);
     }
 
     private RoleJpa getOrCreateRole(String name) {
---
+++ api-user/src/main/java/backend/finance/api_user/infrastructure/gateways/CustomerQueryGateway.java
@@ -0,0 +1,40 @@
+package backend.finance.api_user.infrastructure.gateways;
+
+import backend.finance.api_user.application.dtos.internal.CustomerDto;
+import backend.finance.api_user.infrastructure.ports.output.CustomerQueryOutputPort;
+import backend.finance.api_user.infrastructure.presenters.CustomerPresenter;
+import backend.finance.api_user.infrastructure.repositories.CustomerRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+@RequiredArgsConstructor
+public class CustomerQueryGateway implements CustomerQueryOutputPort {
+
+    private final CustomerRepository customerRepository;
+
+    @Transactional(readOnly = true)
+    @Override
+    public Optional<CustomerDto> findById(UUID id) {
+        return customerRepository.findById(id)
+                .map(CustomerPresenter::toCustomerDto);
+    }
+
+    @Transactional(readOnly = true)
+    @Override
+    public Optional<CustomerDto> findByEmail(String email) {
+        return customerRepository.findByEmail(email)
+                .map(CustomerPresenter::toCustomerDto);
+    }
+
+    @Transactional(readOnly = true)
+    @Override
+    public Optional<CustomerDto> findByUsername(String username) {
+        return customerRepository.findByUserUsername(username)
+                .map(CustomerPresenter::toCustomerDto);
+    }
+}
