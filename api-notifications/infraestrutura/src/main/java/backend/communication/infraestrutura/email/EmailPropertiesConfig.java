6d2e8f24fa4067011e5319ab2290288424c1b171
Junior Martins <junior.soares.martins@proton.me>
2025-12-04 16:14:11 -0400

    DOCS: melhoria no tutorial de Mail.

--- api-notifications/infraestrutura/src/main/java/backend/communication/infraestrutura/email/EmailPropertiesConfig.java
+++ api-notifications/infraestrutura/src/main/java/backend/communication/infraestrutura/email/EmailPropertiesConfig.java
@@ -1,13 +1,11 @@
 package backend.communication.infraestrutura.email;
 
 import lombok.Getter;
-import lombok.Setter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @Getter
-@Setter
 public class EmailPropertiesConfig {
 
     @Value("${spring.mail.username}")
